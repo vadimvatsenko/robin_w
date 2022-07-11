b090baeb4299257f751664f051a83e23d5f5802b
Vadim Vatsenko <vadim.vatsenko@gmail.com>
2022-07-11 18:09:05 +0300

    11.06 18:08

--- src/index.js
+++ src/index.js
@@ -14,4 +14,26 @@
     refs.body.classList.toggle('no-scroll');
   }
 })();
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector("[data-modal-open]"),
+    closeModalBtn: document.querySelector("[data-modal-close]"),
+    modal: document.querySelector("[data-modal]"),
+  };
 
+  refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.closeModalBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle("is-hidden");
+  }
+})();
+
+// Слайдер
+
+$('.autoplay').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
